497a7c6f8bbc38a3d56256b789146f57a14fde7d
Lukas Ant <lukas.ant@hotmail.com>
2019-02-18 17:18:31 +0700

    Added notification service and notificationQueue

---
+++ src/app/notification.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotificationService {
+
+  public notificationQueue: any[] = [];
+
+  constructor() { }
+
+  addNotification(typeClass, heading, body, addition = '') {
+    this.notificationQueue.push({
+      notificationTypeClass: typeClass,
+      notificationHeading: heading,
+      notificationBody: body,
+      notificationAddition: addition
+    })
+  }
+
+}
--- src/app/notification/notification.component.ts
+++ src/app/notification/notification.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NotificationService } from '../notification.service';
 
 @Component({
   selector: 'app-notification',
@@ -7,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotificationComponent implements OnInit {
 
-  constructor() { }
+  // alert-primary
+  // alert-secondary
+  // alert-success
+  // alert-danger
+  // alert-warning
+  // alert-info
+  // alert-light
+  // alert-dark
+
+  notificationQueue: any[];
+
+  constructor(
+    private notificationService: NotificationService
+  ) {  }
 
   ngOnInit() {
+    this.notificationQueue = this.notificationService.notificationQueue;
+
+    this.notificationService.addNotification(
+      'alert-dark',
+      'MyStudentHouse Notification',
+      'This is an important MyStudentHouse notification.',
+      'This is an important notification addittion.'
+    );
+
+    this.notificationService.addNotification(
+      'alert-success',
+      'MyStudentHouse Notification 2',
+      'Success'
+    )
+
   }
 
 }
